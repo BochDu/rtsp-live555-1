c32293d804f0c94275a7d6e12d75c32019cb3bed
BochDu <dbc_924@outlook.com>
2025-03-31 11:26:01 +0800

    feature(rtsp):过程实现rtsp视频流 增加rtsp说明和ssl库安装

--- README.md
+++ README.md
@@ -20,6 +20,11 @@ make
 ## 软件下载
 [VLC播放器下载](https://www.videolan.org/vlc/index.zh_CN.html)
 
+## OpenSSL库安装
+```
+sudo apt-get install libssl-dev
+```
+
 ## live555库移植
 
 - [live555源码下载](www.live555.com/liveMedia/)
@@ -39,3 +44,15 @@ cd ./live/testProgs
 ```
 - 在`/live/testProgs/`目录放入视频文件`test.264`，仓库已经`resources`复制该文件
 - 可执行文件会读`test.264`启动视频流，在`VLC播放器`选择`媒体`,`打开网络串流`播放器输入IP播放
+
+## RTSP协议
+
+RTSP全称实时流协议（Real Time Streaming Protocol）它是一个网络控制协议，设计用于娱乐、会议系统中控制流媒体服务器。RTSP用于在希望通讯的两端建立并控制媒体会话（session），客户端通过发出VCR-style命令如play、record、pause等来实时控制媒体流。在线实时观看情景下，传统的HTTP下载显然不能够匹配该需求，因此在寻求streaming的道路上，RTSP脱颖而出。
+
+- RTSP协议：负责服务器与客户端之间的请求与响应
+- RTP协议：负责传输媒体数据
+- RTCP协议：在RTP传输过程中提供传输信息
+
+## live555库知识
+
+
--- rtsp/rtsp_server.cpp
+++ rtsp/rtsp_server.cpp
@@ -1,8 +1,17 @@
 #include "liveMedia.hh"
 #include "BasicUsageEnvironment.hh"
+#include "H264VideoFileServerMediaSubsession.hh"
 
 #include <iostream>
 #include <cassert>
+
+#define CODE_MODE 1
+#define CODE_MODE_CHECK(x) (x == CODE_MODE)
+
+#define CONFIG_TEST_LIVE555 0
+#define CONFIG_RTSP_PROCESS 1
+
+#if CODE_MODE_CHECK(CONFIG_TEST_LIVE555)
 
 // 封装创建任务调度器的函数
 TaskScheduler *createTaskScheduler()
@@ -59,4 +68,49 @@ int main(int argc, char **argv)
 {
     if (!check_live555())
         assert(true);
+
+    return 0;
 }
+
+#elif CODE_MODE_CHECK(CONFIG_RTSP_PROCESS)
+
+// 主函数
+int main(int argc, char **argv)
+{
+    // 创建一个任务调度器
+    TaskScheduler *scheduler = BasicTaskScheduler::createNew();
+
+    // 创建一个使用环境
+    UsageEnvironment *env = BasicUsageEnvironment::createNew(*scheduler);
+
+    // 打印一条消息，表明任务调度器已创建
+    *env << "Live555 任务调度器已创建！\n";
+
+    // 创建一个媒体服务器
+    RTSPServer *rtspServer = RTSPServer::createNew(*env, 8554);
+    if (rtspServer == NULL)
+    {
+        *env << "无法创建 RTSP 服务器: " << env->getResultMsg() << "\n";
+        return 1;
+    }
+
+    // 创建一个媒体会话
+    ServerMediaSession *sms = ServerMediaSession::createNew(*env, "testStream", "Test H.264 Stream");
+
+    // 添加 H.264 视频子会话
+    sms->addSubsession(H264VideoFileServerMediaSubsession::createNew(*env, "../resources/test.264", false));
+
+    // 向 RTSP 服务器添加媒体会话
+    rtspServer->addServerMediaSession(sms);
+
+    // 打印 RTSP URL
+    *env << "可以通过以下 URL 访问该流: " << rtspServer->rtspURL(sms) << "\n";
+
+    // 进入事件循环
+    env->taskScheduler().doEventLoop();
+
+    return 0;
+}
+
+#else
+#endif
